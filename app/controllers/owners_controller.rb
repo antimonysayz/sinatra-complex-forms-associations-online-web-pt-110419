105ef1a6e0192337128dee01fd8b83a7f3ed968e
Anthony Mai <anthony.mai530@gmail.com>
2020-03-02 04:27:11 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -29,7 +29,7 @@ class OwnersController < ApplicationController
     erb :'/owners/show'
   end
 
-  post '/owners/:id' do
+  post '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
     @owner.update(params["owner"])
     if !params["pet"]["name"].empty?
