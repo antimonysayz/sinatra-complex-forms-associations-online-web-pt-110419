ab9554d47ddd324634497b8e8385646b565124e5
Anthony Mai <anthony.mai530@gmail.com>
2020-03-02 04:27:53 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -46,10 +46,6 @@ end
 
   post '/owners/:id' do
     @owner = Owner.find(params[:id])
-    @owner.update(params["owner"])
-    if !params["pet"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pet"]["name"])
-    end
     redirect to "owners/#{@owner.id}"
   end
 end
