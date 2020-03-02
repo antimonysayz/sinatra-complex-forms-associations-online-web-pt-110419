0794f7b2c4f89f3650cbb0a2c2673e939844d707
Anthony Mai <anthony.mai530@gmail.com>
2020-03-02 03:09:26 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -34,7 +34,6 @@ class OwnersController < ApplicationController
     if !params[:owner].keys.include?("pet_ids")
     params[:owner]["pet_ids"] = []
     end
-    #######
 
     @owner = Owner.find(params[:id])
     @owner.update(params["owner"])
